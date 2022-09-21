5827e4c89bf1ef2db953860bf953dd7e8af7e156
Brittany Joiner <brittanyjoiner15@gmail.com>
2022-09-21 20:49:09 +0100

    add techstack

---
+++ components/TechStackCard.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Image from "next/image";
+
+const renderTags = (tags) => {
+  return tags.map((tag) => {
+    return (
+      <span
+        className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+        key={tag}
+      >
+        {tag}
+      </span>
+    );
+  });
+};
+
+export default function ToolCard(tool) {
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg m-5 p-5">
+      <Image src={tool.logo} alt={`${tool.name} logo`} width={96} height={96} />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{tool.name}</div>
+        <p className="text-gray-700 text-base">{tool.description}</p>
+        <button className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded mt-2 w-full">
+          Try It Out
+        </button>
+      </div>
+
+      <div className="px-6 pt-4 pb-2">{renderTags(tool.tags)}</div>
+    </div>
+  );
+}
---
+++ pages/techstack.js
@@ -0,0 +1,30 @@
+import React from "react";
+import ToolCard from "../components/TechStackCard";
+import { techStack, myToolsStack } from "../components/Data";
+import Header from "../components/Header";
+
+export const renderCards = (techStack) => {
+  return techStack.map((tool) => {
+    return <ToolCard {...tool} key={tool.name} />;
+  });
+};
+
+// can i combine componets? what if this uses polaris like tech out my stack
+
+export default function TechStackList() {
+  return (
+    <>
+      <Header />
+
+      <div className="text-2xl bold mx-5">What have I built?</div>
+      {/* // Do I wanna break this out by tool? Use case? Something else? Or just use tags? */}
+      <div className="grid grid-cols-1 md:grid-cols-4 m-5 p-5 rounded overflow-hidden shadow-lg">
+        {renderCards(myToolsStack)}
+      </div>
+      <div className="text-2xl bold mx-5">What&apos;s in my Toolbox?</div>
+      <div className="grid grid-cols-1 md:grid-cols-4 m-5 p-5 rounded overflow-hidden shadow-lg">
+        {renderCards(techStack)}
+      </div>
+    </>
+  );
+}
